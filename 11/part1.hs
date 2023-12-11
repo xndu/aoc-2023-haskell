ff576b797a7b35a99434f47f5c4c3960dbed40df
aduan <102342448+xndu@users.noreply.github.com>
2023-12-11 00:17:12 -0800

    day 11

---
+++ 11/part1.hs
@@ -0,0 +1,7 @@
+import Data.List
+main = do
+    input <- readFile "input.txt"
+    let cmapi f x = concat $ zipWith f x [0..]
+    let diffs l = zipWith (-) (tail l) l
+    let adj l = sum $ zipWith (*) [i * (length l - i) | i <- [1..]] $ map (\d -> (if d>1 then 2 else 1)*(d-1)+1) $ diffs $ sort l
+    print $ sum $ map adj $ transpose $ cmapi (\l i -> cmapi (\e j -> [[i,j] | e=='#']) l) $ lines input
