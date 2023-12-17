44641b193e4cae7f58db3a3db34a5c7d2f76711f
aduan <102342448+xndu@users.noreply.github.com>
2023-12-17 02:35:59 -0800

    day 16

---
+++ 16/part1.2.hs
@@ -0,0 +1,21 @@
+import Data.List
+import Data.Tuple
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+zipWt f (a,b) (c,d) = (f a c, f b d)
+main = do
+    input <- readFile "input.txt"
+    let cave = M.fromList $ concat $ zipWith (\i -> zipWith (\j -> ((i,j),)) [0..]) [0..] $ lines input
+    let dir = [(1,0),(-1,0),(0,1),(0,-1)]
+    let nexts = M.fromList $ M.toList cave >>= \(l,e) -> map (\d -> (
+                (l,d),
+                S.fromList $ filter ((`M.member` cave) . fst) $ map (\r -> (zipWt (+) l r, r)) $ case e of
+                    '.' -> [d]
+                    '\\'-> [swap d]
+                    '/' -> [refl $ swap d]
+                    '|' -> delete (refl d) [(1,0),(-1,0)]
+                    '-' -> delete (refl d) [(0,1),(0,-1)])) dir
+            where refl (x,y) = (-x,-y)
+    let energized s = length $ S.map fst $ fst $ until (null . snd) (\(b,f) -> (S.union b f, foldl (\a e -> S.union a $ nexts M.! e) S.empty f S.\\ b)) (S.empty, s)
+    print $ energized $ S.singleton ((0,0), (0,1))
